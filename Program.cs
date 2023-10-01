4df343b34dc5adda8c1a5ff51ecd24789fa7551d
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-10-01 03:34:27 -0800

    Thread Work example

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using _004_ThreadsExample;
+
+Console.SetWindowSize(80, 40);
+
+var work1 = new ThreadWork(ConsoleColor.Red);
+var work2 = new ThreadWork(ConsoleColor.Yellow);
+
+work1.Priority = System.Threading.ThreadPriority.Highest;
+work2.Priority = System.Threading.ThreadPriority.Lowest;
+
+work1.BeginInvoke();
+work2.BeginInvoke();
+
+work1.EndInvoke();
+work2.EndInvoke();
+
+Console.WriteLine();
+Console.WriteLine("Thread " + work1.RunThread.Name + "can count " + work1.Count);
+Console.WriteLine("Thread " + work2.RunThread.Name + "can count " + work2.Count);
+
+
---
+++ ThreadWork.cs
@@ -0,0 +1,43 @@
+﻿namespace _004_ThreadsExample
+{
+    public class ThreadWork
+    {
+        public Thread RunThread;
+        public bool _stop;
+        public readonly ConsoleColor _color;
+
+        public int Count { get; set; }
+
+        public ThreadWork( ConsoleColor color)
+        {
+            RunThread = new Thread(Run);
+            this._color = color;
+            Console.ForegroundColor = this._color;
+            Console.WriteLine("Thread " + RunThread.Name + " started");
+        }
+
+        public void Run()
+        {
+            do
+            {
+                ++Count;
+            }
+            while (_stop == false && Count < 20000000);
+        }
+
+        public void BeginInvoke()
+        {
+            RunThread.Start();
+        }
+
+        public void EndInvoke()
+        {
+            RunThread.Join();
+        }
+
+        public ThreadPriority Priority
+        {
+            set { RunThread.Priority = value; }
+        }
+    }
+}
